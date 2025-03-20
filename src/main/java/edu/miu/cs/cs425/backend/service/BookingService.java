69a0bd463982e49bf315356d5f5fa31c9806f83a
vmercel <vmercel@gmail.com>
2025-03-20 04:57:19 -0500

    uploaded backend

---
+++ src/main/java/edu/miu/cs/cs425/backend/service/BookingService.java
@@ -0,0 +1,43 @@
+package edu.miu.cs.cs425.backend.service;
+
+import edu.miu.cs.cs425.backend.application.command.CreateBookingCommand;
+import edu.miu.cs.cs425.backend.application.commandhandler.BookingCommandHandler;
+import edu.miu.cs.cs425.backend.application.query.FlightSearchQuery;
+import edu.miu.cs.cs425.backend.application.query.GetFlightQuery;
+import edu.miu.cs.cs425.backend.application.queryhandler.FlightQueryHandler;
+import edu.miu.cs.cs425.backend.application.queryhandler.FlightSearchQueryHandler;
+import edu.miu.cs.cs425.backend.domain.entity.FlightSearchResult;
+import edu.miu.cs.cs425.backend.domain.entity.Booking;
+import edu.miu.cs.cs425.backend.domain.entity.Flight;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class BookingService {
+    private final BookingCommandHandler bookingCommandHandler;
+    private final FlightQueryHandler flightQueryHandler;
+    private final FlightSearchQueryHandler flightSearchQueryHandler;
+
+    public BookingService(BookingCommandHandler bookingCommandHandler, FlightQueryHandler flightQueryHandler,
+                          FlightSearchQueryHandler flightSearchQueryHandler) {
+        this.bookingCommandHandler = bookingCommandHandler;
+        this.flightQueryHandler = flightQueryHandler;
+        this.flightSearchQueryHandler = flightSearchQueryHandler;
+    }
+
+    public Booking createBooking(CreateBookingCommand command) {
+        return bookingCommandHandler.handle(command);
+    }
+
+    public Optional<Flight> getFlight(String flightId) {
+        GetFlightQuery query = new GetFlightQuery();
+        query.setFlightId(flightId);
+        return flightQueryHandler.handle(query);
+    }
+
+    public FlightSearchResult searchFlights(FlightSearchQuery query) {
+        return flightSearchQueryHandler.handle(query);
+    }
+}
